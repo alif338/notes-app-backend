950621a0058d67fbc42122796d25390581ef8eeb
alif338 <alifm26072000@gmail.com>
2022-03-05 21:11:34 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    /**
+     * plugin: {
+        name: 'notes',
+        version: '1.0.0',
+        register: async (server, { service, validator }) => {
+          const notesHandler = new NotesHandler(service, validator);
+          server.route(routes(notesHandler));
+        },
+      }
+     */
+    options: {
+      service: notesService,
+      validator: NotesValidator
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
